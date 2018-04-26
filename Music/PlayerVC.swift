46e3102318541eb41381fa5c67f21d0e95939d5d
Michael Buss Andersen <michaelbussandersen@gmail.com>
2018-04-26 10:21:49 +0200

    Removed old PlayerVC because it was incompatible with the new for some reason. Kept a local copy for future reference

--- Music/PlayerVC.swift
+++ Music/PlayerVC.swift
@@ -1,42 +1,48 @@
 //
-//  ViewController.swift
+//  PlayerVC.swift
 //  Music
 //
-//  Created by Michael Buss Andersen on 24/03/2018.
+//  Created by Michael Buss Andersen on 26/04/2018.
 //  Copyright © 2018 NoobLabs. All rights reserved.
 //
 
 import UIKit
-import MediaPlayer
 
 class PlayerVC: UIViewController {
 
-    @IBOutlet weak var scrubber: UISlider!
-    // Instantiate a new music player
-    
-    let myMediaPlayer = MPMusicPlayerApplicationController.applicationQueuePlayer
-    
-    @IBAction func scrubberAction(_ sender: Scrubber) {
-        // print("Scrubber changed value")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        scrubber.translatesAutoresizingMaskIntoConstraints = true
-        scrubber.setThumbImage(#imageLiteral(resourceName: "Thumb"), for: .normal) // Sets Thumb image on scrubber
 
-        // Add a playback queue containing all songs on the device
-         myMediaPlayer.setQueue(with: MPMediaQuery.songs())
-        // Start playing from the beginning of the queue
-         myMediaPlayer.play()
-        
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
+    @IBAction func play(_ sender: Any) {
+        print("Play Pressed")
+//        animatePlayerHeight(to: 300, withDuration: 0.5)
+    }
+    
+    
+    private func animatePlayerHeight(to height: Int, withDuration duration: Double) {
+        UIView.animate(withDuration: duration) {
+            self.animatePlayerHeight(to: 300, withDuration: 0.5)
+            self.view.layoutIfNeeded()
+        }
     }
 
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
